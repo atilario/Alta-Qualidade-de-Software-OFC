1ee373bd440509b18899c9b46ecc5a33e83d9f2f
atilario <atilamascarenhas67@gmail.com>
2026-08-11 10:37:53 -0300

    Initial commit with project files

---
+++ learning_py/hello_world.py
@@ -0,0 +1,6 @@
+def hello_world():
+    """this function prints a hello"""
+    print("hello world!")
+
+
+hello_world()
---
+++ learning_py/is_even.py
@@ -0,0 +1,8 @@
+def even_or_odd(x):
+    if x % 2 != 0:
+        print("Is odd")
+    else:
+        print("Is even")
+
+x = int(input())
+even_or_odd(x)
